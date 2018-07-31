5882e6da1cf46f014c9b09c133c1613eab6e78ec
aahmed2006 <league@wts20.attlocal.net>
2018-07-31 12:01:59 -0700

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
-	}
+Robot r2d2 = new Robot();
+r2d2.setPenColor(Color.RED);
+r2d2.penDown();
+r2d2.sparkle();
+r2d2.setSpeed(100);
+for(int i = 0; i < 4; i++)	{
+	r2d2.move(100);
+	r2d2.turn(90);
+}
+}
 }
